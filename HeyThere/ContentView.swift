5018e72b774479bf32a5e89f61aa91b9ca86e327
silvana <garciaserrano.1@osu.edu>
2019-10-10 14:45:16 -0400

    Initial Commit

---
+++ HeyThere/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeyThere
+//
+//  Created by Silvana Garcia on 10/10/19.
+//  Copyright © 2019 Silvana Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
