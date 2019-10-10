4bbfd1e457a496c35def2a543b62eb2726d29d9b
silvana <garciaserrano.1@osu.edu>
2019-10-10 14:54:58 -0400

    first commit

---
+++ HeyThere/View Controllers/ChatRoom/ChatRoomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatRoomViewController.swift
+//  HeyThere
+//
+//  Created by Silvana Garcia on 10/10/19.
+//  Copyright © 2019 Silvana Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
